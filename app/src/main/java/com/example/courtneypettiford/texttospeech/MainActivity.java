74408b5dbfb65dfbd38e515415734f654aef4c05
Courtney Pettiford <cpettiford3@gatech.edu>
2018-07-25 23:13:05 -0400

    first commit

---
+++ app/src/main/java/com/example/courtneypettiford/texttospeech/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.courtneypettiford.texttospeech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Locale;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech textToSpeech;
+    private EditText inputText;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submit = findViewById(R.id.submitButton);
+        inputText = findViewById(R.id.editText);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toSpeak = inputText.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+}
